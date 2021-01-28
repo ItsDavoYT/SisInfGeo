acc960139723085368ee3b6e78d5988502ff046a
ItsDavoYT <drc412@gmail.com>
2021-01-28 10:26:25 -0600

    Primeros programas

---
+++ p01holaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace p01holaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!!");
+        }
+    }
+}
+
